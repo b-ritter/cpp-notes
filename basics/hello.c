c3412826151fad1d8087512dbf24a14996eed166
RITTER <br440r@att.com>
2017-10-22 09:26:02 -0500

    Start out with a basic hello world

---
+++ basics/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// From Kernighan and Ritchie
+// Compiled on Windows with MinGW
+// Compile with
+//     g++ hello.c -o hello.out
+// Run with
+//     ./hello.out
+
+main() {
+    // Every C program must have main somewhere
+    printf("hello, world\n");
+}
